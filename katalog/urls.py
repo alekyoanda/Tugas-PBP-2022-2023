64b908160a26f8fde95efefa6b50b70bfb2408f3
Alek Yoanda <alexyoandatampu1@gmail.com>
2022-09-09 23:21:12 +0700

    Finishing tugas2

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,1 +1,9 @@
 # TODO: Implement Routings Here
+from django.urls import path
+from . import views
+
+app_name = 'katalog'
+
+urlpatterns = [
+    path('', views.index, name="index")
+]
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
 # TODO: Create your views here.
+from .models import CatalogItem
+def index(request):
+    catalog_items = CatalogItem.objects.all()
+    context = {
+        'name': 'Alek Yoanda Partogi.T',
+        'id': '2106750276',
+        'catalogs': catalog_items
+    }
+    return render(request, 'katalog.html', context)
