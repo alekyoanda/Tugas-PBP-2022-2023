cd28f4a72cf4ac698e51f79c113a860508c9b98e
Alek Yoanda <alexyoandatampu1@gmail.com>
2022-09-12 14:47:58 +0700

    Adding testing unit

--- katalog/tests.py
+++ katalog/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
-
+from django.test import SimpleTestCase, TestCase, Client
+from django.urls import reverse, resolve
+from .import views
 # Create your tests here.
+class TestUrls(SimpleTestCase):
+    def test_index_url_is_resolved(self):
+        url = reverse('katalog:index')
+        self.assertEquals(resolve(url).func, views.index)
+
+class TestViews(TestCase):
+    def test_katalog_list_GET(self):
+        client = Client()
+        response = client.get(reverse('katalog:index'))
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'base.html')
